7d068e393c9a786b4a479d220cd6c758093140ee
Vansh <sainivansh8116@gmail.com>
2026-03-23 17:04:39 +0530

    Landing page Done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { Route,BrowserRouter as Router, Routes } from 'react-router-dom'
+import LandingPage from './pages/landing'
+
+function App() {
+
+  return (
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage/>}/>
+          </Routes>
+        </Router>
+      </div>
+  )
+}
+
+export default App;
