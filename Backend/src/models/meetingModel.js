fa4f4cfab017c8a475f79352c9a62b7d49ab2400
Vansh <sainivansh8116@gmail.com>
2026-03-26 22:49:30 +0530

    Prototype

--- Backend/src/models/meetingModel.js
+++ Backend/src/models/meetingModel.js
@@ -1,11 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 
-const meetingSchema = Schema({
-    user_id:{type:String},
-    meeting_code:{type:String, required:true},
-    date:{type: Date, default: Date.now , required:true}
-})
+const meetingSchema = new Schema(
+    {
+        user_id: { type: String },
+        meetingCode: { type: String, required: true },
+        date: { type: Date, default: Date.now, required: true }
+    }
+)
 
 const Meeting = mongoose.model("Meeting", meetingSchema);
-export {Meeting};
+
+export { Meeting };
